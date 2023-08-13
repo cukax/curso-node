ab9d12bce6c16e915c716cc326b1102521bf0eae
Erick Sanchez <cukas_12@hotmail.com>
2023-08-13 13:45:21 -0600

    Sign in google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
 
-const { response } = require('express');
+const { request, response } = require('express');
 const { generarJWT } = require('../helpers/generar-jwt');
 
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async ( req, res = response ) => {
 
@@ -47,6 +48,53 @@ const login = async ( req, res = response ) => {
 
 }
 
+const googleSignIn = async (req = request, res = response ) => {
+
+    const { id_token } = req.body;
+
+    try {
+        
+        const { nombre, img, correo } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            const data = {
+                nombre, 
+                correo,
+                password: 'Prueba',
+                img,
+                google: true,
+                rol: 'USER_ROLE'
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msj: 'Hable con el administrador, usuario bloqueado' 
+            });
+        }
+
+        const token = await generarJWT( usuario.id );
+        
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        });
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
